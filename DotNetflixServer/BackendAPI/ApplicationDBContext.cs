65258ec82c9c27a86ca114a05be98e93ca3989f2
Guryanov Vadim <vadim.guryanov.2004@mail.ru>
2023-03-13 22:31:38 +0300

    Migrations fix

--- DotNetflixServer/BackendAPI/ApplicationDBContext.cs
+++ DotNetflixServer/BackendAPI/ApplicationDBContext.cs
@@ -13,7 +13,6 @@ namespace BackendAPI
 		public ApplicationDBContext(DbContextOptions<ApplicationDBContext> options)
 			: base(options)
 		{
-			Database.EnsureCreated();
 		}
 
 		protected override void OnModelCreating(ModelBuilder modelBuilder)
