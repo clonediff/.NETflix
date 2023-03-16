7cd454c33725f6baae8c9a10d260a79b398b82d9
clonediff <32036592+clonediff@users.noreply.github.com>
2023-03-16 18:45:47 +0300

    [mod] Change BusinessLogic DB Models

--- DotNetflixServer/DBModels/BusinessLogic/Fees.cs
+++ DotNetflixServer/DBModels/BusinessLogic/Fees.cs
@@ -3,8 +3,11 @@
 	public class Fees
 	{
 		public int Id { get; set; }
+		public int? WorldId { get; set; }
 		public CurrencyValue? World { get; set; }
-		public CurrencyValue? Russia { get; set; } 
+		public int? RussiaId { get; set; }
+		public CurrencyValue? Russia { get; set; }
+		public int? USAId { get; set; }
 		public CurrencyValue? USA { get; set; }
 	}
 }
--- DotNetflixServer/DBModels/BusinessLogic/Types.cs
+++ DotNetflixServer/DBModels/BusinessLogic/Types.cs
@@ -3,7 +3,7 @@
 	public class Types
 	{
 		public int Id { get; set; }
-		public string Name { get; set; }	// maxLength = 20
-		public string Slug { get; set; }	// maxLength = 20
+		public string Name { get; set; }    // maxLength = 20
+		public string Slug { get; set; }    // maxLength = 20
 	}
 }
