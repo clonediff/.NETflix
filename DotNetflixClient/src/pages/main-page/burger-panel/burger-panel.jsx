cf8735bb53f65ac3e7cb788d9a29187dd6f2161f
NailZinn <nailzinnurov17@gmail.com>
2023-03-23 21:06:36 +0300

    fix adaptive burger panel links

--- DotNetflixClient/src/pages/main-page/burger-panel/burger-panel.jsx
+++ DotNetflixClient/src/pages/main-page/burger-panel/burger-panel.jsx
@@ -3,16 +3,16 @@ import './burger-panel.css'
 const BurgerPanel = () => {
     return (
         <div className='burger-panel'>
-            <a href='/'>
+            <a href='/search?type=movie'>
                 Фильмы
             </a>
-            <a href='/'>
+            <a href='/search?type=tv-series'>
                 Сериалы
             </a>
-            <a href='/'>
+            <a href='/search?type=cartoon'>
                 Мультфильмы
             </a>
-            <a href='/'>
+            <a href='/search?type=anime'>
                 Аниме
             </a>
         </div>
