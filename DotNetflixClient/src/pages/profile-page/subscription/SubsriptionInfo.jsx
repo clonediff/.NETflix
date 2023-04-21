e79acb02fea6168d0f9924ec36d6f3c65febd11b
Guryanov Vadim <vadim.guryanov.2004@mail.ru>
2023-04-21 18:32:02 +0300

    Added modal window for payment operations. Added service for payment simulation

--- DotNetflixClient/src/pages/profile-page/subscription/SubsriptionInfo.jsx
+++ DotNetflixClient/src/pages/profile-page/subscription/SubsriptionInfo.jsx
@@ -1,5 +1,12 @@
+import { useState } from "react"
+import { PaymentForm } from "../main-info/components/payment-form/payment-form"
+
 export const SubscriptionInfo = () => {
+    const [showModal, setShowModal] = useState(false)
     return (
+        <>
         <p>Пока здесь пусто</p>
-    )
+        <PaymentForm Product={{"Cost": 23, "Name": "Simple", "Id": 0}} Show={showModal} SetShow={setShowModal}></PaymentForm>
+        <button onClick={()=>{setShowModal(true)}}></button>
+    </>)
 }
---
+++ DotNetflixServer/Services/PaymentService/PaymentService.cs
@@ -0,0 +1,31 @@
+﻿using DtoLibrary;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Services.PaymentService
+{
+    public class PaymentService : IPaymentService
+    {
+        static bool ValidateCredentialsFormat(CardDataDto cardCredentials)
+        {
+            if(!Regex.IsMatch(cardCredentials.CardNumber, @"\d\d*"))
+                return false;
+
+            return (cardCredentials.CVV_CVC >= 100 && cardCredentials.CVV_CVC <= 999);
+        }
+
+        public bool PayByCard(CardDataDto cardCredentials, decimal amount)
+        {
+            if(!ValidateCredentialsFormat(cardCredentials))
+                return false;
+
+            //payment logic
+
+            return true;
+        }
+    }
+}
