64b24e87ef8d8fee8905bfefa1e79e612a59056b
NailZinn <nailzinnurov17@gmail.com>
2023-10-14 14:44:13 +0300

    added infrastructure for cqrs

---
+++ DotNetflixServer/DotNetflix.Abstractions/Cqrs/ICommandHandler.cs
@@ -0,0 +1,15 @@
+﻿using MediatR;
+
+namespace DotNetflix.Abstractions.Cqrs;
+
+public interface ICommandHandler<in TCommand> : IRequestHandler<TCommand>
+    where TCommand : ICommand
+{
+    
+}
+
+public interface ICommandHandler<in TCommand, TResponse> : IRequestHandler<TCommand, TResponse>
+    where TCommand : ICommand<TResponse>
+{
+    
+}
