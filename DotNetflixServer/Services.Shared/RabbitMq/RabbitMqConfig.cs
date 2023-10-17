a1a341be9a51a915611605dc1bf548a2604b7beb
clonediff <32036592+clonediff@users.noreply.github.com>
2023-10-17 10:10:03 +0300

    [fix] comments

--- DotNetflixServer/Services.Shared/RabbitMq/RabbitMqConfig.cs
+++ DotNetflixServer/Services.Shared/RabbitMq/RabbitMqConfig.cs
@@ -4,9 +4,9 @@ public class RabbitMqConfig
 {
     public const string SectionName = "RabbitMqConfig";
     
-    public string Username { get; set; }
-    public string Password { get; set; }
-    public string Hostname { get; set; }
+    public string Username { get; set; } = default!;
+    public string Password { get; set; } = default!;
+    public string Hostname { get; set; } = default!;
     public int Port { get; set; }
     
     public string FullHostname => $"amqp://{Username}:{Password}@{Hostname}:{Port}";
