a0f008960deaa5600122e4002861c5807f2a1a2f
Ilfatt <salavatyllinilfat@mail.ru>
2023-11-18 13:15:28 +0300

    did task - 88

---
+++ DotNetflixServer/Services.Infrastructure/S3/IS3Storage.cs
@@ -0,0 +1,47 @@
+namespace Services.Infrastructure.S3;
+
+public interface IS3Storage
+{
+    /// <summary>
+    /// Созать бакет
+    /// </summary>
+    /// <param name="bucketIdentifier">уникальный идентификатор бакета</param>
+    public Task CreateBucket(string bucketIdentifier);
+
+    /// <summary>
+    /// Удалить бакет
+    /// </summary>
+    /// <param name="bucketIdentifier">уникальный идентификатор бакета</param>
+    /// <returns></returns>
+    public Task RemoveBucket(string bucketIdentifier);
+
+    /// <summary>
+    /// Проверить существует ли бакет 
+    /// </summary>
+    /// <param name="bucketIdentifier">уникальный идентификатор бакета</param>
+    /// <returns>true если существует</returns>
+    public Task<bool> BucketExist(string bucketIdentifier);
+
+    /// <summary>
+    /// Положить файл в бакет
+    /// </summary>
+    /// <param name="fileStream">Файл в виде <see cref="Stream"/></param>
+    /// <param name="fileIdentifier">уникальный идентификатор файла</param>
+    /// <param name="bucketIdentifier">уникальный идентификатор бакета</param>
+    public Task PutFileInBucket(Stream fileStream, string fileIdentifier, string bucketIdentifier);
+
+    /// <summary>
+    /// Удалить файл из бакета
+    /// </summary>
+    /// <param name="fileIdentifier">уникальный идентификатор файла</param>
+    /// <param name="bucketIdentifier">уникальный идентификатор бакета</param>
+    public Task RemoveFileInBucket(string fileIdentifier, string bucketIdentifier);
+    
+    /// <summary>
+    /// Получить файл из бакета
+    /// </summary>
+    /// <param name="fileIdentifier">уникальный идентификатор файла</param>
+    /// <param name="bucketIdentifier">уникальный идентификатор бакета</param>
+    /// <returns>Файл в виде <see cref="Stream"/> или null если бакет или файл не найден. </returns>
+    public Task<Stream?> GetFileFromBucket(string fileIdentifier, string bucketIdentifier);
+}
---
+++ DotNetflixServer/Services.Infrastructure/S3/MinioS3Storage.cs
@@ -0,0 +1,83 @@
+using Minio;
+using Minio.DataModel.Args;
+using Minio.Exceptions;
+
+namespace Services.Infrastructure.S3;
+
+public class MinioS3Storage
+{
+    private readonly IMinioClient _minioClient;
+
+    public MinioS3Storage(IMinioClient minioClient)
+    {
+        _minioClient = minioClient;
+    }
+    
+    public async Task CreateBucket(string bucketIdentifier)
+    {
+        var args = new MakeBucketArgs()
+            .WithBucket(bucketIdentifier);
+
+        await _minioClient.MakeBucketAsync(args);
+    }   
+
+    public async Task RemoveBucket(string bucketIdentifier)
+    {
+        var args = new RemoveBucketArgs()
+            .WithBucket(bucketIdentifier);
+
+        await _minioClient.RemoveBucketAsync(args);
+    }
+
+    public async Task<bool> BucketExist(string bucketIdentifier)
+    {
+        var args = new BucketExistsArgs()
+            .WithBucket(bucketIdentifier);
+
+        return await _minioClient.BucketExistsAsync(args);
+    }
+
+    public async Task PutFileInBucket(Stream fileStream, string fileIdentifier, string bucketIdentifier)
+    {
+        var args = new PutObjectArgs()
+            .WithBucket(bucketIdentifier)
+            .WithStreamData(fileStream)
+            .WithObject(fileIdentifier)
+            .WithObjectSize(fileStream.Length);
+
+        await _minioClient.PutObjectAsync(args);
+    }
+
+    public async Task RemoveFileFromBucket(string fileIdentifier, string bucketIdentifier)
+    {
+        var args = new RemoveObjectArgs()
+            .WithBucket(bucketIdentifier)
+            .WithObject(fileIdentifier);
+
+        await _minioClient.RemoveObjectAsync(args);
+    }
+
+    public async Task<Stream?> GetFileFromBucket(string fileIdentifier, string bucketIdentifier)
+    {
+        var response = new MemoryStream();
+        var args = new GetObjectArgs()
+            .WithBucket(bucketIdentifier)
+            .WithObject(fileIdentifier)
+            .WithCallbackStream(stream => stream.CopyTo(response));
+
+        try
+        {
+            await _minioClient.GetObjectAsync(args);
+        }
+        catch (ObjectNotFoundException)
+        {
+            return null;
+        }
+        catch (BucketNotFoundException)
+        {
+            return null;
+        }
+
+        return response;
+    }
+}
