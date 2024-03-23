0456263c7789dc224095828822c343ae292eae15
NailZinn <nailzinnurov17@gmail.com>
2024-03-23 13:26:47 +0300

    added payment service

---
+++ DotNetflixServer/DotNetflix.Payment/Services/PaymentService.cs
@@ -0,0 +1,26 @@
+﻿using Grpc.Core;
+
+namespace DotNetflix.Payment.Services;
+
+public class PaymentService : Payment.PaymentService.PaymentServiceBase
+{
+    public override async Task<Response> Pay(CardData request, ServerCallContext context)
+    {
+        await Task.Delay(2000, context.CancellationToken);
+
+        return new Response
+        {
+            Success = true
+        };
+    }
+
+    public override async Task<Response> Refund(CardData request, ServerCallContext context)
+    {
+        await Task.Delay(2000, context.CancellationToken);
+        
+        return new Response
+        {
+            Success = true
+        };
+    }
+}
