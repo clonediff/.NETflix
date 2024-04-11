3607c80456a840bab800b195d4f001ab5c07299a
NailZinn <nailzinnurov17@gmail.com>
2024-04-11 22:49:22 +0300

    added search dialog

--- mobile/lib/widgets/header.dart
+++ mobile/lib/widgets/header.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:mobile/constants/colors.dart';
+import 'package:mobile/widgets/search.dart';
 
 class Header extends StatelessWidget implements PreferredSizeWidget {
   const Header({ super.key });
@@ -24,7 +25,12 @@ class Header extends StatelessWidget implements PreferredSizeWidget {
         IconButton(
           icon: const Icon(Icons.search, size: 26),
           color: Colors.white,
-          onPressed: () { },
+          onPressed: () {
+            showDialog(
+              context: context, 
+              builder: (context) => const SearchDialog()
+            );
+          },
         ),
       ],
       backgroundColor: DotNetflixColors.headerBackgroundColor,
